bc0b641e2a358faf98594e174fa4b991d8e84ba8
s117987@student.hb.se <s117987@student.hb.se@6a0e5fe6-5da2-a6d8-fd71-225fc879c7c8>
2012-03-12 21:18:12 +0000

    Nu är nog hela telnet delen färdig

--- trunk/dkomm-grupp5-sa11/dkomm-grupp5-sa11/telnet.cpp
+++ trunk/dkomm-grupp5-sa11/dkomm-grupp5-sa11/telnet.cpp
@@ -1,5 +1,5 @@
 ////  Server.cpp : Defines the entry point for the console application.
-////
+//// Kalle dp9UP8Bg6PZ6 låt stå :P
 //
 #include <stdio.h>
 #include <iostream>
@@ -75,112 +75,118 @@ int _tmain(int argc, _TCHAR* argv[]){
 	// Vänta på inkommande anrop
 	struct sockaddr clientAddr;
 	int clientAddrLen = sizeof(clientAddr);
-	while(on){
-	int iResult;
-	SOCKET s1 = accept(s,&clientAddr,&clientAddrLen);
-	if(s1 != INVALID_SOCKET) {
-		char hostName[100];
-		char portName[100];
-		bool loggedIn = false;
-		int ok = getnameinfo(&clientAddr,clientAddrLen,hostName,100,portName,100,NI_NUMERICSERV );
-		if(ok == 0) {
-			printf("Accept incoming from: %s at port %s\n",hostName,portName);
-		}
+	while(on)
+	{
+		int iResult;
+		SOCKET s1 = accept(s,&clientAddr,&clientAddrLen);
+		if(s1 != INVALID_SOCKET) {
+			char hostName[100];
+			char portName[100];
+			bool loggedIn = false;
+			int ok = getnameinfo(&clientAddr,clientAddrLen,hostName,100,portName,100,NI_NUMERICSERV );
+			if(ok == 0) {
+				printf("Accept incoming from: %s at port %s\n",hostName,portName);
+			}
 
-		// Läsa av 
-		string cmd;
-		string arg;
-		char line[100];
-		char *temp = (char *)malloc(1);
+			// Läsa av 
+			string cmd;
+			string arg;
+			char line[100];
+			char *temp = (char *)malloc(1);
 
-		int i = 0;
-		send(s1,"Welcome, enter password to log in. press escape to exit\n",56, 1);
-		while(true){
-			while(*temp != '\n'){
-				iResult = recv(s1, temp, 1, 0);
+			int i = 0;
+			send(s1,"Welcome, enter password to log in. press escape to exit\n\r",57, 1);
+			while(true){
+				while(*temp != '\n'){
+					iResult = recv(s1, temp, 1, 0);
+					if(*temp == 27) //escape stänger telnetklienten och återgår till att lyssna på s1
+						break;
+					line[i] = *temp;
+				i++;
+				}
 				if(*temp == 27) //escape stänger telnetklienten och återgår till att lyssna på s1
-					break;
-				line[i] = *temp;
-			i++;
-			}
-			if(*temp == 27) //escape stänger telnetklienten och återgår till att lyssna på s1
-					break;
-			*temp = ' ';
-			int j = sscanf(line, "%s", cmd.c_str());
-			// lägger till ett \0 på slutet =)
-			cmd = cmd.c_str();
-			int hej = cmd.compare("password");
-			if ((cmd.compare("password") == 0) && !loggedIn){
-				int j = sscanf(line, "password %s",arg.c_str());
-				arg = arg.c_str();
-				if(arg.compare("hemligt") == 0){
-					loggedIn = true;
-					send(s1,"You are now logged in\n",22, 1);
+						break;
+				*temp = ' ';
+				int j = sscanf(line, "%s", cmd.c_str());
+				// lägger till ett \0 på slutet =)
+				cmd = cmd.c_str();
+				int hej = cmd.compare("password");
+				if ((cmd.compare("password") == 0) && !loggedIn){
+					int j = sscanf(line, "password %s",arg.c_str());
+					arg = arg.c_str();
+					if(arg.compare("hemligt") == 0){
+						loggedIn = true;
+						send(s1,"You are now logged in\n\r",23, 1);
+					}
+					else {
+						loggedIn = false;
+						send(s1,"Invalid password\n\r",18, 1);
+					}
 				}
-				else {
-					loggedIn = false;
-					send(s1,"Invalid password\n",17, 1);
+				else if (cmd.compare("status") == 0 && loggedIn){
+					if (on)
+						send(s1,"Server is on\n\r",14,1);
+					else
+						send(s1,"Server is off\n\r",15,1);
 				}
-			}
-			else if (cmd.compare("status") == 0 && loggedIn){
-				if (on)
-					send(s1,"Server is on\n",13,1);
-				else
-					send(s1,"Server is off\n",14,1);
-			}
-			else if (cmd.compare("loggfile") == 0 && loggedIn){
-				FILE *infile;
-				char ch;
-				string ch2;
+				else if (cmd.compare("loggfile") == 0 && loggedIn){
+					FILE *infile;
+					char ch;
+					string ch2;
 	
-				infile = fopen("config.txt","r");
-				while(infile != NULL)
-				{
-					ch = fgetc(infile);
-					ch2 = ch;
-					if(ch != EOF)
+					infile = fopen("config.txt","r");
+					while(infile != NULL)
 					{
-						send(s1,ch2._Myptr(), 1, 1);
-						if(ch == '\n')
+						ch = fgetc(infile);
+						ch2 = ch;
+						if(ch != EOF)
 						{
-							ch2 = "\r";
 							send(s1,ch2._Myptr(), 1, 1);
+							if(ch == '\n')
+							{
+								ch2 = "\r";
+								send(s1,ch2._Myptr(), 1, 1);
+							}
+						}
+						else 
+						{
+							send(s1,"\n\r", 2, 1);
+							break;
 						}
 					}
 				}
-			}
-			else if (cmd.compare("server") == 0 && loggedIn){
-				int j = sscanf(line, "server %s",arg.c_str());
-				arg = arg.c_str();
-				if(arg.compare("off") == 0){
-					on = false;
-					send(s1,"Webserver is now off\n",21,1);
+				else if (cmd.compare("server") == 0 && loggedIn){
+					int j = sscanf(line, "server %s",arg.c_str());
+					arg = arg.c_str();
+					if(arg.compare("off") == 0){
+						on = false;
+						send(s1,"Webserver is now off\n\r",22,1);
+					}
+					else if(arg.compare("on") == 0){
+						on = true;
+						send(s1,"Webserver is now on\n\r",21,1);
+					}
+					else send(s1,"Invalid arguement (on/off)\n\r",28, 1);
 				}
-				else if(arg.compare("on") == 0){
-					on = true;
-					send(s1,"Webserver is now on\n",20,1);
-				}
-				else send(s1,"Invalid arguement (on/off)\n",27, 1);
+				else if(!loggedIn)
+					send(s1, "Enter password first\n\r",22,1);
+				else
+					send(s1, "Invalid command\n\r",17,1);
+				i = 0;
+				cmd = "\0";
+				arg = "\0";
 			}
-			else if(!loggedIn)
-				send(s1, "Enter password first\n",21,1);
-			else
-				send(s1, "Invalid command\n",16,1);
-			i = 0;
-			cmd = "\0";
-			arg = "\0";
-		}
-		loggedIn = false;
+			loggedIn = false;
 
-		//free(buffer);
-		// Stäng sockets
-		closesocket(s1);
+			//free(buffer);
+			// Stäng sockets
+			closesocket(s1);
 		
-	}
-	else {
-		int err = WSAGetLastError();
-		printf("%d\n",err);
-	}
+		}
+		else {
+			int err = WSAGetLastError();
+			printf("%d\n",err);
+		}
 	}
 	closesocket(s);
 
